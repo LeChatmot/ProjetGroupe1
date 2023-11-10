3db89ef3fc2a8e776acd3c515e30330d476c1ed8
enzovilaca59 <vilacaenzo59@gmail.com>
2023-11-10 16:41:59 +0100

    page-recette

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 include 'header.php';
 include 'page-recette.php';
 include 'footer.php';
+?>
--- page-recette.php
+++ page-recette.php
@@ -2,39 +2,42 @@
 $_SESSION ['recettes'] = [
         [
             'nom' => 'test',
-	        'temps de preparation' => 10,
-	        'temps de cuisson' => 15,
-	        'niveau de difficulté' => 'Accessible',
-	        'nombre de personnes' => 4,
-	        'ingredients' => 'Tomate',
+	        'tempsDePreparation' => 10,
+	        'tempsDeCuisson' => 15,
+	        'niveauDeDifficulté' => 'Accessible',
+	        'nombreDePersonnes' => 4,
+	        'ingredients' => ['id'],
             'quantitéUnité' => 5,
             'quantitéPoids' => 400,
         ],
     [
         'nom' => 'test',
-        'temps de preparation' => 10,
-        'temps de cuisson' => 15,
-        'niveau de difficulté' => 'Accessible',
-        'nombre de personnes' => 4,
-        'ingredients' => 'Tomate',
+        'tempsDePreparation' => 10,
+        'tempsDeCuisson' => 15,
+        'niveauDeDifficulté' => 'Accessible',
+        'nombreDePersonnes' => 4,
+        'ingredients' => ['id'],
         'quantitéUnité' => 5,
         'quantitéPoids' => 400,
     ],
 ];
-var_dump($_SESSION);
 ?>
 
 <h1>Page recette</h1>
-
-<?php foreach ($_SESSION ['recettes'] as $key => $value) {
-    echo "
-        <div class='card' style='width: 18rem;'>
-            <img src='https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png' class='card-img-top' alt='...'>
-            <div class='card-body'>
-                <h5 class='card-title'>Card title</h5>
-                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href='#' class='btn btn-primary'>Go somewhere</a>
+<div  style=" display: flex; flex-wrap: wrap" >
+    <?php foreach ($_SESSION ['recettes'] as $key => $value) {
+        echo "    
+            <div class='card d-flex flex-row' style='margin: 10px'>
+                <div class='card-body'>
+                    <h5 class='card-title'>{$value['nom']}</h5>
+                    <div>
+                        <p class='card-text'>Temps de préparation : {$value['tempsDePreparation']} min.</p>
+                        <p class='card-text'>Temps de cuisson : {$value['tempsDeCuisson']} min.</p>
+                        <p class='card-text'>Difficulté : {$value['niveauDeDifficulté']}.</p>
+                    </div>
+                    <a href='#' class='btn btn-primary'>Go somewhere</a>
+                </div>
             </div>
-        </div>
-    ";
-}
+            ";
+    }?>
+</div>
